371bc1db5c3d61a928d2c2994a351fce3e3ba9c9
rickyega <rickyega@yahoo.ne.jp>
2025-04-27 00:48:36 +0200

    fix surface rule

--- fabric/src/main/java/com/ricky/terrariamod/world/ModTerrablenderAPI.java
+++ fabric/src/main/java/com/ricky/terrariamod/world/ModTerrablenderAPI.java
@@ -1,15 +1,15 @@
-//package com.ricky.terrariamod.world;
-//
-//import com.ricky.terrariamod.Constants;
-//import com.ricky.terrariamod.world.biome.ModSurfaceRules;
-//import terrablender.api.SurfaceRuleManager;
-//import terrablender.api.TerraBlenderApi;
-//
-//public class ModTerrablenderAPI implements TerraBlenderApi {
-//    @Override
-//    public void onTerraBlenderInitialized(){
-////         表層のルールを登録
-//        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, Constants.MOD_ID,
-//                ModSurfaceRules.makeCustomSurfaceRules());
-//    }
-//}
+package com.ricky.terrariamod.world;
+
+import com.ricky.terrariamod.Constants;
+import com.ricky.terrariamod.world.biome.ModSurfaceRules;
+import terrablender.api.SurfaceRuleManager;
+import terrablender.api.TerraBlenderApi;
+
+public class ModTerrablenderAPI implements TerraBlenderApi {
+    @Override
+    public void onTerraBlenderInitialized(){
+//         表層のルールを登録
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, Constants.MOD_ID,
+                ModSurfaceRules.makeCustomSurfaceRules());
+    }
+}
